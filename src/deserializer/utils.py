f2658c120c08feb398da7d078837ee6340d4a9d4
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2024-09-11 15:21:08 +0800

    Add support for `type` statement added in 3.12
    Also unified param names

--- src/deserializer/utils.py
+++ src/deserializer/utils.py
@@ -2,45 +2,60 @@
 # pyright: reportUnknownArgumentType=false
 # pyright: reportUnknownVariableType=false
 # pyright: reportAny=false
+# pyright: reportDeprecated=information
 
 import inspect
 from types import UnionType
+from typing import Union
 from typing import Any
 from typing import Literal
 from typing import Callable
 from typing import TypeGuard
 from typing import get_args
 from typing import get_origin
+from typing import TypeAliasType
 from deserializer.defines import JsonType
 from deserializer.defines import NonArgumentConstructorProtocol
 
 
-CheckerType = Callable[[object], TypeGuard[type[NonArgumentConstructorProtocol]]]
+CheckerType = Callable[[Any], TypeGuard[type[NonArgumentConstructorProtocol]]]
 
 
-def _is_type_args_infinite(t: type) -> bool:
-    return t is Literal or t is UnionType or t is tuple
+def _is_container_empty(i: object) -> bool:
+    if isinstance(i, (list, set, dict)):
+        return len(i) == 0
+    return False
 
 
-def _is_type_list_like(t: type) -> bool:
+def _is_literal(t: Any) -> bool:
+    return t is Literal
+
+
+def _is_union_type(t: Any) -> bool:
+    return t is UnionType or t is Union
+
+
+def _is_type_args_infinite(t: Any) -> bool:
+    return _is_literal(t) or _is_union_type(t) or t is tuple
+
+
+def _is_type_list_like(t: Any) -> bool:
     return t is list or t is set
 
 
-def _is_type_dict_like(t: type) -> bool:
+def _is_type_dict_like(t: Any) -> bool:
     return t is dict
 
 
-def _is_union_type(i: object) -> TypeGuard[UnionType]:
-    return get_origin(i) is UnionType
-
-
-def _check_generics(i: object, origin: type, args: tuple[Any, ...]) -> bool:
+def _check_generics(i: object, origin: Any, args: tuple[Any, ...]) -> bool:
     if _is_type_args_infinite(origin):
+        if _is_literal(origin):
+            return i in args
         return type(i) in args
     if _is_type_list_like(origin):
-        return is_type_match(i, args[0])
+        return is_type_match(i, args[0]) or _is_container_empty(i)
     if _is_type_dict_like(origin):
-        return is_type_match(i, args[1])
+        return is_type_match(i, args[1]) or _is_container_empty(i)
     raise NotImplementedError("Unsupported type {}".format(origin))
 
 
@@ -75,31 +90,34 @@ def is_valid_json(i: object) -> TypeGuard[JsonType]:
     return False
 
 
-def _is_valid_class(i: object) -> TypeGuard[type[NonArgumentConstructorProtocol]]:
-    if isinstance(i, type) and hasattr(i, "__init__") and callable(i.__init__):
-        signature = inspect.signature(i.__init__)
+def _is_valid_class(t: Any) -> TypeGuard[type[NonArgumentConstructorProtocol]]:
+    if isinstance(t, type) and hasattr(t, "__init__") and callable(t.__init__):
+        signature = inspect.signature(t.__init__)
         return _is_parameters_valid(*signature.parameters.values())
     return False
 
 
 def to_valid_class(
-    i: object,
+    t: Any,
     checker: CheckerType = _is_valid_class,
 ) -> type[NonArgumentConstructorProtocol]:
     """Convert type given to NonArgumentConstructor."""
-    if checker(i):
-        return i
-    if _is_union_type(i):
-        args = get_args(i)
+    if checker(t):
+        return t
+    if _is_union_type(t):
+        args = get_args(t)
         for arg in args:
             if checker(arg):
                 return arg
         raise ValueError("No type matches checker can be extracted from UnionType.")
-    raise NotImplementedError("Unsupported object {}".format(i))
+    raise NotImplementedError("Unsupported object {}".format(t))
 
 
-def is_type_match(i: object, t: type) -> bool:
+def is_type_match(i: object, t: Any) -> bool:
     """Check if object matches type given."""
+    if isinstance(t, TypeAliasType):
+        # If type is defined with `type` statement added in 3.12
+        t = t.__value__
     args = get_args(t)
     if len(args) > 0:
         origin = get_origin(t)
